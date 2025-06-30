f2c1a7c517770667b6cc19b94b5955b9bc0f1030
Kasun Wilbagedara <kasunwilbagedara2001@gmail.com>
2025-06-30 22:00:10 +0530

    nav bar,home page set

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='text-green-500'> 
-      testing tailwind
+    <div className='mx-4 sm:mx-[10%]'> 
+    <Navbar/>
+     <Routes>
+    <Route path='/' element={<Home/>}/>
+   
+       
+
+     </Routes>
     </div>
   )
 }
