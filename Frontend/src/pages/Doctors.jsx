4ec2c9290e460716b634acd5b33cad7c2b8918b3
Kasun Wilbagedara <kasunwilbagedara2001@gmail.com>
2025-07-15 23:59:06 +0530

    lawyer pg draft1

--- Frontend/src/pages/Doctors.jsx
+++ Frontend/src/pages/Doctors.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
+
+const{ speciality } = useParams()
+
+const[filterDoc,setFilterDoc] = useState([])
+
+const{doctors}= useContext(AppContext)
+
   return (
     <div>
-      
+      <p>Browse through the docotrs specialist</p>
+    <div>
+      <div>
+        
+        <p>speal1</p>
+        <p>speal2</p>
+        <p>speal3</p>
+        <p>speal4</p>
+        <p>speal5</p>
+        <p>speal6</p>
+      </div>
+      <div>
+        {
+
+        }
+      </div>
     </div>
+   
+    </div>
+    
   )
 }
 
